a719e68642e0f0b14e450047db05165d7af9f544
Justin Kilpatrick <justin@althea.net>
2020-10-01 07:26:00 -0400

    Add new peggy message types
    This includes oracle types as well as batch requests and confirms
    
    I'm concerned the nested structs in the oracle type won't work out
    so well given our current serialization but that will just have to
    be tested out.

--- src/msg.rs
+++ src/msg.rs
@@ -19,7 +19,6 @@ pub struct SendMsg {
 pub struct SetEthAddressMsg {
     #[serde(rename = "address")]
     pub eth_address: EthAddress,
-    #[serde(rename = "validator")]
     pub validator: Address,
     /// a hex encoded string representing the Ethereum signature
     #[serde(rename = "signature")]
@@ -34,12 +33,94 @@ pub struct ValsetRequestMsg {
 #[cfg(feature = "peggy")]
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
 pub struct ValsetConfirmMsg {
-    #[serde(rename = "validator")]
     pub validator: Address,
-    #[serde(rename = "nonce")]
     pub nonce: Uint256,
     #[serde(rename = "signature")]
     pub eth_signature: String,
+}
+
+/// a transaction we send to move funds from Cosmos to Ethereum
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct SendToEthMsg {
+    pub sender: Address,
+    pub dest_address: EthAddress,
+    pub send: Coin,
+    pub bridge_fee: Coin,
+}
+
+/// This message requests that a batch be created on the Cosmos chain, this
+/// may or may not actually trigger a batch to be created depending on the
+/// internal batch creation rules. Said batch will be of arbitrary size also
+/// depending on those rules. What this message does determine is the coin
+/// type of the batch. Since all batches only move a single asset within them.
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct RequestBatchMsg {
+    pub requester: Address,
+    pub denom: String,
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct ConfirmBatchMsg {
+    pub nonce: Uint256,
+    pub validator: Address,
+    /// a hex encoded string representing the Ethereum signature
+    #[serde(rename = "signature")]
+    pub eth_signature: String,
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct ERC20Token {
+    amount: Uint256,
+    symbol: String,
+    #[serde(rename = "token_contract_address")]
+    token_contract_address: EthAddress,
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct EthereumBridgeDepositClaim {
+    pub nonce: Uint256,
+    pub validator: Address,
+    /// a hex encoded string representing the Ethereum signature
+    #[serde(rename = "signature")]
+    pub eth_signature: String,
+    #[serde(rename = "ERC20Token")]
+    pub erc20_token: ERC20Token,
+    pub ethereum_sender: EthAddress,
+    pub cosmos_receiver: Address,
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct EthereumBridgeWithdrawBatchClaim {
+    pub nonce: Uint256,
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct EthereumBridgeMultiSigUpdateClaim {
+    pub nonce: Uint256,
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Hash)]
+pub enum EthereumBridgeClaim {
+    EthereumBridgeDepositClaim(EthereumBridgeDepositClaim),
+    EthereumBridgeMultiSigUpdateClaim(EthereumBridgeMultiSigUpdateClaim),
+    EthereumBridgeWithdrawBatchClaim(EthereumBridgeWithdrawBatchClaim),
+}
+
+#[cfg(feature = "peggy")]
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct CreateEthereumClaimsMsg {
+    pub ethereum_chain_id: Uint256,
+    pub bridge_contract_address: EthAddress,
+    pub validator: Address,
+    pub claims: Vec<EthereumBridgeClaim>,
 }
 
 /// Any arbitrary message
@@ -48,15 +129,35 @@ pub struct ValsetConfirmMsg {
 pub enum Msg {
     #[serde(rename = "cosmos-sdk/MsgSend")]
     SendMsg(SendMsg),
+
     #[cfg(feature = "peggy")]
     #[serde(rename = "peggy/MsgSetEthAddress")]
     SetEthAddressMsg(SetEthAddressMsg),
+
     #[cfg(feature = "peggy")]
     #[serde(rename = "peggy/MsgValsetRequest")]
     ValsetRequestMsg(ValsetRequestMsg),
+
     #[cfg(feature = "peggy")]
     #[serde(rename = "peggy/MsgValsetConfirm")]
     ValsetConfirmMsg(ValsetConfirmMsg),
+
+    #[cfg(feature = "peggy")]
+    #[serde(rename = "peggy/MsgSendToEth")]
+    SendToEthMsg(SendToEthMsg),
+
+    #[cfg(feature = "peggy")]
+    #[serde(rename = "peggy/MsgRequestBatch")]
+    RequestBatchMsg(RequestBatchMsg),
+
+    #[cfg(feature = "peggy")]
+    #[serde(rename = "peggy/MsgConfirmBatch")]
+    ConfirmBatchMsg(ConfirmBatchMsg),
+
+    #[cfg(feature = "peggy")]
+    #[serde(rename = "peggy/MsgCreateEthereumClaims")]
+    CreateEthereumClaimsMsg(CreateEthereumClaimsMsg),
+
     #[serde(rename = "deep_space/Test")]
     Test(String),
 }
