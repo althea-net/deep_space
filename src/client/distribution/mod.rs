12edaf57cd1baf1cb7cc5be9141a535678961694
Justin Kilpatrick <justin@althea.net>
2021-09-26 18:23:09 -0400

    Fix community pool endpoint for DecCoins
    DecCoins are the solution provided by Cosmos for dealing with very small
    amounts of tokens, allowing things like the community pool to pull a
    fraction of a token from transactions without being erased in integer
    rounding errors. The community pool query endpoint returns them,
    presenting a problem for us trying to compare them to normal coins.

--- src/client/distribution/mod.rs
+++ src/client/distribution/mod.rs
@@ -5,20 +5,27 @@ use crate::error::CosmosGrpcError;
 use crate::{Coin, Contact};
 use cosmos_sdk_proto::cosmos::distribution::v1beta1::query_client::QueryClient as DistQueryClient;
 use cosmos_sdk_proto::cosmos::distribution::v1beta1::QueryCommunityPoolRequest;
+use num256::Uint256;
+use num_bigint::ParseBigIntError;
+
+// required because dec coins are multiplied by 1*10^18
+const ONE_ETH: u128 = 10u128.pow(18);
 
 impl Contact {
-    /// Gets a list of coins in the community pool
+    /// Gets a list of coins in the community pool, note returned values from this endpoint
+    /// are in DecCoins for precision, for the sake of ease of use this endpoint converts them
+    /// into their normal form, for easy comparison against any other coin or amount.
     pub async fn get_community_pool_coins(&self) -> Result<Vec<Coin>, CosmosGrpcError> {
         let mut grpc = DistQueryClient::connect(self.url.clone()).await?;
         let res = grpc.community_pool(QueryCommunityPoolRequest {}).await?;
         let val = res.into_inner().pool;
         let mut res = Vec::new();
         for v in val {
-            let parse_result = v.amount.parse();
+            let parse_result: Result<Uint256, ParseBigIntError> = v.amount.parse();
             match parse_result {
                 Ok(parse_result) => res.push(Coin {
                     denom: v.denom,
-                    amount: parse_result,
+                    amount: parse_result / ONE_ETH.into(),
                 }),
                 Err(e) => return Err(CosmosGrpcError::ParseError { error: e }),
             }
