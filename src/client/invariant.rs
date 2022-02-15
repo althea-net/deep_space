3a54064c9852736f50add6e74cd9478a39314bfc
Christian Borst <christianoborst@gmail.com>
2022-02-15 13:00:51 -0600

    Update version for invariants release

--- src/client/invariant.rs
+++ src/client/invariant.rs
@@ -1,15 +1,14 @@
-use std::time::Duration;
+use crate::error::CosmosGrpcError;
+use crate::{Coin, Contact, Msg, PrivateKey};
 use cosmos_sdk_proto::cosmos::base::abci::v1beta1::TxResponse;
 use cosmos_sdk_proto::cosmos::crisis::v1beta1::MsgVerifyInvariant;
 use cosmos_sdk_proto::cosmos::tx::v1beta1::SimulateResponse;
-use crate::{Coin, Contact, Msg, PrivateKey};
-use crate::error::CosmosGrpcError;
+use std::time::Duration;
 
 pub const MSG_VERIFY_INVARIANT_TYPE: &str = "/cosmos.crisis.v1beta1.MsgVerifyInvariant";
 
 impl Contact {
-    /// A utility function which simulates the specified invariant and returns whether the invariant
-    /// is violated or not
+    /// A utility function which simulates the specified invariant and returns the given SimulationResponse
     ///
     /// # Arguments
     /// * `module_name` - The module containing the invariant to check
@@ -17,22 +16,15 @@ impl Contact {
     /// * `private_key` - A private key used to sign and send the transaction
     ///
     /// # Examples
-    /// TODO: Finish the examples
     /// ```rust
     /// use cosmos_sdk_proto::cosmos::bank::v1beta1::MsgSend;
     /// use cosmos_sdk_proto::cosmos::tx::v1beta1::BroadcastMode;
     /// use deep_space::{Coin, client::Contact, Fee, MessageArgs, Msg, PrivateKey};
     /// use std::time::Duration;
     /// let private_key = PrivateKey::from_secret("mySecret".as_bytes());
-    /// let public_key = private_key.to_public_key("cosmospub").unwrap();
-    /// let address = public_key.to_address();
-    /// let coin = Coin {
-    ///     denom: "validatortoken".to_string(),
-    ///     amount: 1u32.into(),
-    /// };
     /// let contact = Contact::new("https:://your-grpc-server", Duration::from_secs(5), "prefix").unwrap();
     /// // future must be awaited in tokio runtime
-    /// contact.invalid_method_name(coin.clone(), Some(coin), address, None, private_key);
+    /// contact.invariant_check("gravity", "module-balance", private_key);
     /// ```
     pub async fn invariant_check(
         &self,
@@ -46,18 +38,15 @@ impl Contact {
         let verify = MsgVerifyInvariant {
             sender: our_address.to_string(),
             invariant_module_name: module_name.to_string(),
-            invariant_route: invariant_name.to_string()
+            invariant_route: invariant_name.to_string(),
         };
         let msg = Msg::new(MSG_VERIFY_INVARIANT_TYPE, verify);
         trace!("Submitting simulation");
-        self.simulate_tx(
-            &[msg],
-            private_key,
-        ).await
+        self.simulate_tx(&[msg], private_key).await
     }
 
-    /// A utility function which executes the specified invariant and returns whether the invariant
-    /// is violated or not
+    /// A utility function which executes the specified invariant and returns the TxResponse if one is given
+    /// NOTE: In testing this method does not actually halt the chain due to a cosmos-sdk bug
     ///
     /// # Arguments
     /// * `module_name` - The module containing the invariant to check
@@ -66,7 +55,6 @@ impl Contact {
     /// * `private_key` - A private key used to sign and send the transaction
     ///
     /// # Examples
-    /// TODO: Finish the examples
     /// ```rust
     /// use cosmos_sdk_proto::cosmos::bank::v1beta1::MsgSend;
     /// use cosmos_sdk_proto::cosmos::tx::v1beta1::BroadcastMode;
@@ -81,7 +69,7 @@ impl Contact {
     /// };
     /// let contact = Contact::new("https:://your-grpc-server", Duration::from_secs(5), "prefix").unwrap();
     /// // future must be awaited in tokio runtime
-    /// contact.invalid_method_name(coin.clone(), Some(coin), address, None, private_key);
+    /// contact.invariant_halt("gravity", "module-balance", Some(coin), Duration::from_secs(30), private_key);
     /// ```
     pub async fn invariant_halt(
         &self,
@@ -89,7 +77,7 @@ impl Contact {
         invariant_name: &str,
         fee_coin: Option<Coin>,
         wait_timeout: Duration,
-        private_key: PrivateKey
+        private_key: PrivateKey,
     ) -> Result<TxResponse, CosmosGrpcError> {
         trace!("Creating chain-halting invariant transaction");
         let our_address = private_key.to_address(&self.chain_prefix).unwrap();
@@ -97,7 +85,7 @@ impl Contact {
         let verify = MsgVerifyInvariant {
             sender: our_address.to_string(),
             invariant_module_name: module_name.to_string(),
-            invariant_route: invariant_name.to_string()
+            invariant_route: invariant_name.to_string(),
         };
         let msg = Msg::new(MSG_VERIFY_INVARIANT_TYPE, verify);
         trace!("Submitting chain-halting invariant");
@@ -107,6 +95,7 @@ impl Contact {
             &[fee_coin.unwrap_or_default()],
             Some(wait_timeout),
             private_key,
-        ).await
+        )
+        .await
     }
 }
